98c16d3b9d78da12fb9e4c06865d541baef6eb4f
ychae030 <ychae1997@gmail.com>
2024-10-23 23:57:35 +0900

    init

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+import { Noto_Sans_KR } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+export const noto_sans_KR = Noto_Sans_KR({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const gmarket_md = localFont({
+  src: "./GmarketSansMedium.woff",
+  variable: "--font-gmarket-md",
+  display: "swap",
+  weight: "500",
+});
